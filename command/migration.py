b94a1cac534fdc7dfd9ed5bc86e27183f9290015
iamkamleshrangi <iamkamleshrangi@gmail.com>
2019-04-05 17:27:53 +0530

    Add files

---
+++ command/migration.py
@@ -0,0 +1,87 @@
+from pymongo import MongoClient
+import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "company.settings")
+django.setup()
+from roc.models import Company, Charge, Director
+
+#Settings MongoDB
+conn = MongoClient('127.0.0.1', 27017)
+db = conn['roc_data']
+
+def processRoc(cin):
+    cin = "U72900TG2008PTC060114"
+
+    col1 = "roc_director_details"
+    col2 = "roc_director_disqualifieds" #[x]
+    col3 = "roc_defaulters_companies" #[x]
+    col4 = "roc_defaulters_directors"
+    col5 = "roc_defaulters_secretaries"
+    col6 = "roc" #[x]
+    #================Defaulters Companies===========
+    defaulters_company = db['roc_defaulters_companies'].find_one({'cin_number': cin})
+    company_default = 'False'
+    if defaulters_company:
+        company_default = 'True'
+    #===============Director Disqualified===========
+    director_disqualified = db['roc_director_disqualifieds'].find({'cin_number': cin})
+    for director in director_disqualified:
+        print(director)
+
+    #=================Company Data==================
+    company = db['roc'].find_one({'cin_number': cin})
+    #Extract Data
+    cin_number = company.get('cin_number','')
+    company_name = company.get('company_name','')
+
+    city = company.get('roc_code','').replace('RoC-','')
+    registration_number = company.get('registration_number','')
+
+    company_category = company.get('company_category','')
+    company_subcategory = company.get('company_subcategory','')
+
+    company_class = company.get('company_class','')
+    incorporation_date = company.get('incorporation_date','')
+
+    authorised_capital = company.get('authorised_capital','')
+    paid_capital = company.get('paid_capital','')
+
+    registered_address = company.get('registered_address','')
+    listed_type = company.get('listed_type','')
+
+    company_status = company.get('company_status','')
+    members = company.get('members','')
+
+    #Charges
+    charges = company.get('charges','')
+    for charge in charges:
+        sno = charge.get('sno', '')
+        srn = charge.get('srn','')
+        charge_id = charge.get('charge_id','')
+        bank_name = charge.get('charge_holder_name','')
+        amount = charge.get('amount','')
+        address = charge.get('address','')
+
+        creation_date = charge.get('creation_date','')
+        modification_date = charge.get('modification_date','')
+        break
+
+    #To it later
+    signatories = company.get('signatory','')
+    for signatory in signatories:
+        din_pan = signatory['pan']
+        director_name = signatory['name']
+        director_address = signatory['address']
+        designation = signatory['designation']
+        appointment = signatory['appointment']
+        dsc_registered = signatory['dsc_registered']
+        #break
+
+
+def getCin():
+    con = db['roc']
+    for record in con.find({},{'cin_number':1}):
+        cin = record['cin_number']
+        processRoc(cin)
+        break
+
+getCin()
--- roc/models.py
+++ roc/models.py
@@ -3,70 +3,105 @@ from django.db import models
 class Charge(models.Model):
     srn = models.CharField(max_length=30)
     charge_id = models.CharField(max_length=30)
-    name = models.CharField(max_length=100, blank=False)
+    bank_name = models.CharField(max_length=100, blank=False)
     dated = models.DateField(null=True)
     amount = models.IntegerField(null=True)
+    cin_number = models.CharField(max_length=30, blank=False, null=True)
+    creation_date = models.DateField(null=True)
+    modification_date = models.DateField(null=True)
     address = models.TextField()
 
-    #Dates
+    #Dates created and updated
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
+
     def __str__(self):
         return self.name
 
 class Director(models.Model):
-    din_dpin_pan = models.CharField(max_length=30)
+    CHOICES = [('True', 'YES'), ('False', 'NO')]
+
+    din_pan = models.CharField(max_length=30,blank=False)
     name = models.CharField(max_length=100, blank=False)
+    father_lastname = models.CharField(max_length=50, blank=True)
+    cin_number = models.CharField(max_length=30, blank=False, null=True)
     address = models.TextField(blank=True)
     roc_code = models.CharField(max_length=30, blank=True)
     roc_name = models.CharField(max_length=30, blank=True)
     state = models.CharField(max_length=30, blank=True)
+    city = models.CharField(max_length=30, blank=True)
+    designation = models.CharField(max_length=30, blank=True)
+    appointment_date = models.DateField(null=True)
+    dsc_registered = models.CharField(max_length=6, choices=CHOICES, blank=True)
+    dsc_expiry_date = models.DateField(null=True)
+
+    #Validity Date
+    date_of_birth = models.DateField(null=True)
     vaild_from = models.DateField(null=True)
     vaild_to = models.DateField(null=True)
+
+    #Defaulting Years
+    director_default = models.CharField(max_length=6, choices=CHOICES,blank=True)
+    defaulting_years =  models.CharField(max_length=100, blank=True)
+
+    #addresses
+    address = models.TextField(blank=True)
 
     #Dates
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
+
     def __str__(self):
         return self.name
 
 class Company(models.Model):
-    #Codes
+    #AutoFill DataBase
     CLASS_CHOICES = [('Private', 'Private'), ('Public', 'Public'), ('OPC', 'Private(One Person Company)')]
     LISTED_CHOICES = [('Unlisted','Unlisted'),('Listed','Listed')]
-    DEFAULTER_CHOICES = [('True', 'YES'), ('False', 'NO')]
+    DEFAUL_CHOICES = [('True', 'YES'), ('False', 'NO')]
 
     #ROC Mapping
     cin_number = models.CharField(max_length=30, primary_key=True)
     company_name = models.CharField(max_length=100, blank=False)
 
-    roc_code = models.CharField(max_length=30, blank=True)
-    registration_number = models.CharField(max_length=30, blank=True)
+    #Current Status
+    company_status = models.CharField(max_length=30, blank=True)
 
+    registration_number = models.CharField(max_length=30, blank=True)
+    roc_code = models.CharField(max_length=30, blank=True)
+
+    #Classes
+    company_class = models.CharField(max_length=8, choices=CLASS_CHOICES, blank=False)
+    listed_type = models.CharField(max_length=8, choices=LISTED_CHOICES, blank=True)
     company_category = models.CharField(max_length=80, blank=True)
     company_subcategory = models.CharField(max_length=80, blank=True)
 
-    company_class = models.CharField(max_length=8, choices=CLASS_CHOICES, blank=False)
-
-    listed_type = models.CharField(max_length=8, choices=LISTED_CHOICES, blank=True)
+    #Other Info
     members = models.IntegerField(null=True)
+    stock = models.CharField(max_length=30, blank=True)
+    email_id = models.EmailField(max_length=100,blank=True, unique= False)
 
-    company_status = models.CharField(max_length=30, blank=True)
-
+    #Capital Related
     authorised_capital = models.IntegerField(null=True)
     paid_capital = models.IntegerField(null=True)
 
-    incorporation_date = models.DateField(null=True)
+    #Company Addresses
     registered_address = models.TextField()
+    other_address = models.TextField(blank=True)
 
-    company_default = models.CharField(max_length=6, choices=DEFAULTER_CHOICES, default='False', blank=False)
+    company_default = models.CharField(max_length=6, choices=DEFAUL_CHOICES, default='False', blank=False)
 
     #Dates
+    annual_general_meeting = models.DateField(null=True)
+    balancesheet_date = models.DateField(null=True)
+    incorporation_date = models.DateField(null=True)
+
+    #Dates Update and Create
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     #Relationships
-    directors = models.ForeignKey(Director, null=True)
+    directors = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
     charges = models.ForeignKey(Charge, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
