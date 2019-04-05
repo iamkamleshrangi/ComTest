6f243312a15ec4cef275f098f5466b44f31ed379
iamkamleshrangi <iamkamleshrangi@gmail.com>
2019-04-05 11:44:50 +0530

    change relationship

--- roc/models.py
+++ roc/models.py
@@ -7,6 +7,10 @@ class Charge(models.Model):
     dated = models.DateField(null=True)
     amount = models.IntegerField(null=True)
     address = models.TextField()
+
+    #Dates
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
     def __str__(self):
         return self.name
 
@@ -19,13 +23,15 @@ class Director(models.Model):
     state = models.CharField(max_length=30, blank=True)
     vaild_from = models.DateField(null=True)
     vaild_to = models.DateField(null=True)
+
+    #Dates
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
     def __str__(self):
         return self.name
 
 class Company(models.Model):
-    #Codes 
+    #Codes
     CLASS_CHOICES = [('Private', 'Private'), ('Public', 'Public'), ('OPC', 'Private(One Person Company)')]
     LISTED_CHOICES = [('Unlisted','Unlisted'),('Listed','Listed')]
     DEFAULTER_CHOICES = [('True', 'YES'), ('False', 'NO')]
@@ -38,28 +44,29 @@ class Company(models.Model):
     registration_number = models.CharField(max_length=30, blank=True)
 
     company_category = models.CharField(max_length=80, blank=True)
-    company_subcategory = models.CharField(max_length=80, blank=True) 
+    company_subcategory = models.CharField(max_length=80, blank=True)
 
     company_class = models.CharField(max_length=8, choices=CLASS_CHOICES, blank=False)
 
     listed_type = models.CharField(max_length=8, choices=LISTED_CHOICES, blank=True)
-    members = models.IntegerField(null=True) 
+    members = models.IntegerField(null=True)
 
-    company_status = models.CharField(max_length=30, blank=True) 
+    company_status = models.CharField(max_length=30, blank=True)
 
     authorised_capital = models.IntegerField(null=True)
     paid_capital = models.IntegerField(null=True)
 
     incorporation_date = models.DateField(null=True)
     registered_address = models.TextField()
-    
+
     company_default = models.CharField(max_length=6, choices=DEFAULTER_CHOICES, default='False', blank=False)
 
+    #Dates
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     #Relationships
-    directors = models.ManyToManyField(Director, null=True)
+    directors = models.ForeignKey(Director, null=True)
     charges = models.ForeignKey(Charge, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
